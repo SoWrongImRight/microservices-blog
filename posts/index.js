4ae0730bdd4baeb43352f08ffc7ddf6ef1879cd2
Russ Carroll <info@rcarroll.dev>
2021-06-17 21:22:18 -0400

    Minor refactoring to introduce the Event-Bus servce.  Some difficulty due to the difference between the version presented in the course and the current version.

--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const axios = require('axios');
 
 const cors = require('cors');
 
@@ -14,16 +15,34 @@ app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
-  const id = randomBytes(4).toString('hex');
-  const { title } = req.body;
+app.post('/posts', async (req, res) => {
+  try {
+    const id = randomBytes(4).toString('hex');
+    const { title } = req.body;
 
-  posts[id] = {
-    id,
-    title,
-  };
+    posts[id] = {
+      id,
+      title,
+    };
 
-  res.status(201).send(posts[id]);
+    await axios.post('http://localhost:4005/events', {
+      type: 'PostCreate',
+      data: {
+        id,
+        title,
+      },
+    });
+
+    res.status(201).send(posts[id]);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+app.post('/events', (req, res) => {
+  console.log('Received Event', req.body.type);
+
+  res.send({});
 });
 
 app.listen(4000, () => {
